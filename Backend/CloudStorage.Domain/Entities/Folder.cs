be868a1696c96bb5be49908b0f3eee446941d2fe
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-03-19 22:43:22 +0400

    add domain entities

---
+++ Backend/CloudStorage.Domain/Entities/Folder.cs
@@ -0,0 +1,12 @@
+﻿namespace CloudStorage.Domain.Entities;
+
+public class Folder
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+    public Guid? FolderId { get; set; }
+    public List<Folder>? SubFolders { get; set; }
+    public List<File?> Files { get; set; }
+}
---
+++ Backend/CloudStorage.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace CloudStorage.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public List<Folder?> Folders { get; set; }
+}

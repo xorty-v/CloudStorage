7c914001c3b5a2fabd8ccfee128d72031ef29941
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-03-21 21:41:08 +0400

    Add BaseController with UserId extraction

---
+++ Backend/CloudStorage.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CloudStorage.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BaseController : ControllerBase
+{
+    internal Guid UserId => User.Identity!.IsAuthenticated
+        ? Guid.Parse(HttpContext.User.Claims.FirstOrDefault(c => c.Type == "userId")!.Value)
+        : Guid.Empty;
+}
